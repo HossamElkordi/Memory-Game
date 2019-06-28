9cfea5f6b7c4385434308ebf7264b3ef40139c5d
HossamElkordi <48355657+HossamElkordi@users.noreply.github.com>
2019-06-28 17:21:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,137 @@
+var row = 6;
+var col = 6;
+var flipped = 0;
+var flippedCards = [];
+var matches = 0;
+var started = 0;
+var changed = 0;
+var time = 0;
+var images = [];
+var board = new Array(row*col);
+var check = new Array(row*col);
+
+function setBacks() {
+    var index = 1;
+    for (var i = 0; i <= 27; i++){
+        images[i] = 'emojis/'+ index + '.png';
+        index++;
+    }
+}
+
+function setBoard() {
+    for (var i = 0; i<check.length; i++){
+        check[i] = false;
+    }
+    var index = random(1,row*col);
+    for (var i = 0; i<board.length/2; i++){
+        while (check[index-1] == true){
+            index = random(1,row*col);
+        }
+        board[index-1] = images[i];
+        check[index-1] = true;
+        var index = random(1,row*col);
+        while (check[index-1] == true){
+            index = random(1,row*col);
+        }
+        board[index-1] = images[i];
+        check[index-1] = true;
+    }
+}
+
+function createTable(select, gameTable){
+    var s = document.getElementById(select);
+    switch (s.value){
+        case "66" : row = 6; col = 6; check = new Array(row*col);
+                    board = new Array(row*col); changed = 1;started = 0; matches = 0; break;
+        case "58" : row = 5; col = 8; check = new Array(row*col);
+                    board = new Array(row*col); changed = 1;started = 0; matches = 0; break;
+        case "76" : row = 7; col = 6; check = new Array(row*col);
+                    board = new Array(row*col); changed = 1;started = 0; matches = 0; break;
+        case "86" : row = 8; col = 6; check = new Array(row*col);
+                    board = new Array(row*col); changed = 1;started = 0; matches = 0;  break;
+        case "87" : row = 8; col = 7; check = new Array(row*col);
+                    board = new Array(row*col); changed = 1;started = 0; matches = 0; break;
+    }
+   table(gameTable);
+}
+
+function table(gameTable){
+    setBacks();
+    setBoard();
+    var cellNum = 1;
+    var gameTable = document.getElementById('gameTable');
+    var body = '<table>';
+    for (var i = 0; i < row;  i++){
+        body += '<tr>';
+        for (var j = 0; j < col;  j++){
+            var cell = 'cell-' + cellNum;
+            body += '<td><div class="card"><div class="content" id ='+cell+' onclick="flip(this.id);"><script>timer();</script><div class="front"><img  src='+board[cellNum-1]+'></div><div class="back"><img src="backgrdCard.png"></div></div></button></div></td>'; 
+            cellNum ++;
+        }
+        body += '</tr>';
+    }
+    body += '</table>';
+    gameTable.innerHTML = body;
+}
+
+function flip (cardContent){
+      started ++;
+      if (started == 1){
+          timer();
+      }
+      var content = document.getElementById(cardContent);
+      content.style.transform = "rotateY(180deg)";
+      flippedCards.push(cardContent);
+      flipped++;
+      game();
+}
+
+function backFlip(cardContent){
+    var content= document.getElementById(cardContent);
+    content.style.transform = "rotateY(360deg)";
+}
+
+function cover(){
+    var second = flippedCards.pop();
+    var index1 = second.substring((second.indexOf('-')+1),(second.length));
+    
+    var first = flippedCards.pop();
+    var index2 = first.substring((first.indexOf('-')+1),(first.length));
+    
+    if(board[index1-1] != board[index2-1]){
+        backFlip(first);
+        backFlip(second);
+    }else{
+        matches ++;
+    }
+}
+
+function game (){
+    if(flipped != 2){
+        return;
+    }
+    flipped = 0;
+    window.setTimeout(cover,600);   
+}
+
+function random (min,max){
+    return Math.floor(Math.random()*max)+min;
+}
+
+function timer(){
+    started = 1;
+    if ((started == 1) && (matches != (row*col/2)) && (changed == 0)){
+        window.setTimeout(function(){
+          time++;
+          document.getElementById("time").innerHTML="Your time is : "+ Math.floor(time/60) +":"+time%60;
+            timer();
+            return;
+        },1000);
+    }else{
+        document.getElementById("time").innerHTML="Your time is : "+ Math.floor(time/60) +":"+time%60;
+         changed = 0;
+         time = 0;
+         matches = 0;
+         started = 0;
+    }
+}
